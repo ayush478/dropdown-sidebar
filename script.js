5997db560d59d38adab881b524b252307aced544
Ayush Sunariya <69513008+ayush478@users.noreply.github.com>
2021-08-31 19:12:44 +0530

    fianl changes

--- script.js
+++ script.js
@@ -10,5 +10,5 @@ let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".bx-menu");
 console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", ()=>{
-  sidebar.classList.toggle("close");
+  sidebar.classList.remove("close");
 });
